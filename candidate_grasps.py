4fd5e473a34a8f2db1d350ffcace2d131b79b814
Mohamed Rilah <mohamedrilah22@gmail.com>
2025-07-18 23:46:07 +0100

    Additional Feature(s) Included
    - Added calculation for grasp centre from original depth image
    - Viewing further model outputs, such as orientation and gripper width
    - Timer to estimate duration taken to produce output(s)
    - Image size now in line with training size (300x300)

--- candidate_grasps.py
+++ candidate_grasps.py
@@ -3,6 +3,8 @@ import torch
 import numpy as np 
 import matplotlib.pyplot as plt
 
+import time
+
 from models.ggcnn import GGCNN
 
 def image_preprocessing(image_path = 'box_depthim.png'): 
@@ -12,7 +14,7 @@ def image_preprocessing(image_path = 'box_depthim.png'):
         depth_image = depth_image[:, :, 0]
 
     normalised_depth = (depth_image - np.min(depth_image)) / (np.max(depth_image) - np.min(depth_image))
-    resized_depth = cv2.resize(normalised_depth, (224, 224))
+    resized_depth = cv2.resize(normalised_depth, (300, 300))
     tensor_depth = torch.tensor(resized_depth, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
 
     return tensor_depth
@@ -34,16 +36,27 @@ model.eval()
 
 depth_image_path = 'Images/box_depthim.png'
 
-grasp = get_candidate_grasp(depth_image_path, model)
-print(f'Output from model: ', {grasp})
+start = time.time()
 
-print(grasp[0].shape)
+grasp = get_candidate_grasp(depth_image_path, model)
 
 pos_output = grasp[0].squeeze().cpu().numpy()
-print(f'Shape of tensor 0: {pos_output.shape}')
+cos_output = grasp[1].squeeze().cpu().numpy()
+sin_output = grasp[2].squeeze().cpu().numpy()
+width_output = grasp[3].squeeze().cpu().numpy()
+
 max_index = np.argmax(pos_output)
 y, x = np.unravel_index(max_index, pos_output.shape)
+
+end = time.time()
+
+print(f'Code Runtime: {end-start} seconds')
+
 print(f'Grasp Centre: (x={x}, y={y})')
+print(f'Cos output @ Grasp Centre {cos_output[y,x]}')
+print(f'Sin output @ Grasp Centre {sin_output[y,x]}')
+print(f'Gripper Width @ Grasp Centre {width_output[y,x]}')
+
 
 plt.imshow(pos_output, cmap='hot')
 plt.title('Heatmap for proposed grasp centre point(s)')
@@ -53,6 +66,13 @@ plt.show()
 depth_image = cv2.imread(depth_image_path, cv2.IMREAD_UNCHANGED)
 
 scaled_pos_output = cv2.resize(pos_output, (depth_image.shape[1], depth_image.shape[0]), interpolation=cv2.INTER_LINEAR)
+
+scaled_height = scaled_pos_output.shape[0] / pos_output.shape[0] 
+scaled_width = scaled_pos_output.shape[1] / pos_output.shape[1]
+
+scaled_x = int(x * scaled_width) 
+scaled_y = int(y * scaled_height)
+print(f'Scaled Grasp Centre: (x={scaled_x}, y={scaled_y})')
 
 plt.figure(figsize=(6,4))
 plt.imshow(depth_image, cmap='grey')
